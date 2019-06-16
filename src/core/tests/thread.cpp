d8d122b76bf156c8ff28309a45ceb0617eebd78b
Frédéric Briol <fbriol@cls.fr>
2019-06-16 17:28:08 +0200

    Added the parallel execution pattern.

---
+++ src/core/tests/thread.cpp
@@ -0,0 +1,22 @@
+#include "pyinterp/detail/thread.hpp"
+#include <gtest/gtest.h>
+
+TEST(thread, dispatch) {
+  std::vector<double> src(4096);
+  std::vector<double> dst(4096);
+  for (auto ix = 0; ix < 4096; ++ix) {
+    src[ix] = ix;
+  }
+
+  auto foo = [&dst, &src](size_t start, size_t stop) {
+    for (auto ix = start; ix < stop; ++ix) {
+      dst[ix] = src[ix];
+    }
+  };
+
+  pyinterp::detail::dispatch(foo, src.size(), 0);
+
+  for (auto ix = 0; ix < 4096; ++ix) {
+    EXPECT_EQ(src[ix], dst[ix]);
+  }
+}
