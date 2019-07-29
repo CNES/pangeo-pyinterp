dc04606808a789e7c7eb1e3df0affb42aaf8e8f9
Frédéric Briol <fbriol@cls.fr>
2019-07-29 18:39:33 +0200

    Added the tag of the new version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@ copyright = '(2019, CNES/CLS)'
 author = 'CNES/CLS'
 
 # The short X.Y version
-version = '0.0.2'
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 # -- General configuration ---------------------------------------------------
 
--- src/pyinterp/version.py
+++ src/pyinterp/version.py
@@ -6,8 +6,8 @@ Get software version information
 
 def release(full: bool = False) -> str:
     """Returns the software version number"""
-    # ddab30ba4cf5b5d44a049f62741801df8d44886e
-    result = "0.0.2"
+    # cd4701c9d31dd51b7d28e0a3dfcdd060bb46607c
+    result = "0.0.3"
     if full:
         result += " (29 July 2019)"
     return result
