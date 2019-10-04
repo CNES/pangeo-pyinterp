1c205a904ae2fb99ce09fca7a7fe7b59dbc153ee
Frédéric Briol <fbriol@cls.fr>
2019-10-04 14:13:29 +0200

    Added version number in the root module

--- src/pyinterp/__init__.py
+++ src/pyinterp/__init__.py
@@ -2,9 +2,12 @@
 #
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
+from . import version
 from .core import Axis
 from .grid import Grid2D, Grid3D
 from .rtree import RTree
 from .interpolator.bicubic import bicubic
 from .interpolator.bivariate import bivariate
 from .interpolator.trivariate import trivariate
+__version__ = version.release()
+del version
