668685a1e95f6134e265eb8ff578360a9db368ad
Frédéric Briol <fbriol@cls.fr>
2019-10-04 14:17:31 +0200

    Updating the version number

--- src/pyinterp/version.py
+++ src/pyinterp/version.py
@@ -6,7 +6,7 @@ Get software version information
 
 def release(full: bool = False) -> str:
     """Returns the software version number"""
-    result = "0.0.5"
+    result = "0.0.6"
     if full:
         result += " (04 October 2019)"
     return result
