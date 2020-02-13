9656260d0b2bb7ef91e5653793ac5623306fb3c0
Frédéric Briol <fbriol@gmail.com>
2020-02-13 17:58:58 +0100

    The header of the 'version.py' file is malformed

--- setup.py
+++ setup.py
@@ -145,11 +145,11 @@ def revision():
 
     # Finally, write the file containing the version number.
     with open(module, 'w') as handler:
-        handler.write('''"""
-# Copyright (c) 2020 CNES
+        handler.write('''# Copyright (c) 2020 CNES
 #
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
+"""
 Get software version information
 ================================
 """
