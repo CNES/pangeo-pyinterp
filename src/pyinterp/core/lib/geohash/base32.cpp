7988f2b2687728d4f6e672d38a930475e897df8b
Frédéric BRIOL <fbriol@gmail.com>
2020-09-01 21:15:15 +0200

    VisualStudio bug fix

--- src/pyinterp/core/lib/geohash/base32.cpp
+++ src/pyinterp/core/lib/geohash/base32.cpp
@@ -11,7 +11,7 @@ const char Base32::kInvalid_ = std::numeric_limits<char>::max();
 
 // Encoding characters
 const std::array<char, 32> Base32::encode_ =
-    std::array<char, 32>({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b',
+    std::array<char, 32>{{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b',
                           'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p',
-                          'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'});
+                          'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}};
 }  // namespace pyinterp::geohash
