a2b1d6e4d42bfca8c5ebcf5c489898da3063a843
Frédéric BRIOL <fbriol@gmail.com>
2021-08-21 00:44:52 +0200

    :sparkles: Univariate descriptive statistics

---
+++ src/pyinterp/descriptive_statistics.py
@@ -0,0 +1,171 @@
+from typing import Any, Iterable, Optional, Union
+from numpy.typing import DTypeLike
+import dask.array as da
+import numpy as np
+from . import core
+
+
+def delayed(
+    attr: str,
+    values: da.Array,
+    weights: Optional[da.Array] = None,
+    axis: Optional[Iterable[int]] = None,
+) -> Union[core.DescriptiveStatisticsFloat64,
+           core.DescriptiveStatisticsFloat32]:
+    """Calculate the descriptive statistics of a dask array."""
+    if weights is not None and values.shape != weights.shape:
+        raise ValueError("values and weights must have the same shape")
+
+    def _process_block(attr, x, w, axis):
+        instance = getattr(core, attr)(values=x, weights=w, axis=axis)
+        return np.array([instance], dtype="object")
+
+    drop_axis = list(range(values.ndim))[1:]
+
+    return da.map_blocks(_process_block,
+                         attr,
+                         values,
+                         weights,
+                         axis,
+                         drop_axis=drop_axis,
+                         dtype="object").sum().compute()  # type: ignore
+
+
+class DescriptiveStatistics:
+    """
+    Univariate descriptive statistics.
+
+    Calculates the incremental descriptive statistics from the provided values.
+    The calculation of the statistics is done when the constructor is invoked.
+    Different methods allow to extract the calculated statistics.
+
+    .. seealso::
+
+        Pébay, P., Terriberry, T.B., Kolla, H. et al.
+        Numerically stable, scalable formulas for parallel and online
+            computation of higher-order multivariate central moments
+            with arbitrary weights.
+        Comput Stat 31, 1305–1325,
+        2016,
+        https://doi.org/10.1007/s00180-015-0637-z
+
+    Args:
+        values (numpy.ndarray, dask.Array): Array containing numbers whose
+            statistics are desired.
+        weights (numpy.ndarray, dask.Array, optional): An array of weights
+            associated with the values. If not provided, all values are assumed
+            to have equal weight.
+        axes (iterable, optional): Axis or axes along which to compute the
+            statistics. If not provided, the statistics are computed over the
+            flattened array.    
+    """
+    def __init__(self,
+                 values: Union[da.Array, np.ndarray],
+                 weights: Optional[Union[da.Array, np.ndarray]] = None,
+                 axis: Optional[Iterable[int]] = None,
+                 dtype: Optional[DTypeLike] = None) -> None:
+        dtype = dtype or np.dtype("float64")
+        if dtype == np.dtype("float64"):
+            attr = f"DescriptiveStatisticsFloat64"
+        elif dtype == np.dtype("float32"):
+            attr = f"DescriptiveStatisticsFloat32"
+        else:
+            raise ValueError(f"dtype {dtype} not handled by the object")
+        if isinstance(values, da.Array) or isinstance(weights, da.Array):
+            self._instance = delayed(
+                attr, da.asarray(values),
+                da.asarray(weights) if weights is not None else None, axis)
+        else:
+            self._instance: Union[core.DescriptiveStatisticsFloat64,
+                                  core.DescriptiveStatisticsFloat32] = getattr(
+                                      core, attr)(values, weights, axis)
+
+    def __iadd__(self, other: Any) -> "DescriptiveStatistics":
+        self._instance += other
+        return self
+
+    def count(self) -> np.ndarray:
+        """
+        Returns the count of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the count of samples.
+        """
+        return self._instance.count()
+
+    def kurtosis(self) -> np.ndarray:
+        """
+        Returns the kurtosis of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the kurtosis of samples.
+        """
+        return self._instance.kurtosis()
+
+    def max(self) -> np.ndarray:
+        """
+        Returns the maximum of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the maximum of samples.
+        """
+        return self._instance.max()
+
+    def mean(self) -> np.ndarray:
+        """
+        Returns the mean of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the mean of samples.
+        """
+        return self._instance.mean()
+
+    def min(self) -> np.ndarray:
+        """
+        Returns the minimum of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the minimum of samples.
+        """
+        return self._instance.min()
+
+    def skewness(self) -> np.ndarray:
+        """
+        Returns the skewness of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the skewness of samples.
+        """
+        return self._instance.skewness()
+
+    def sum(self) -> np.ndarray:
+        """
+        Returns the sum of samples.
+        
+        Returns:
+            numpy.ndarray: Returns the sum of samples.
+        """
+        return self._instance.sum()
+
+    def sum_of_weights(self) -> np.ndarray:
+        """
+        Returns the sum of weights.
+        
+        Returns:
+            numpy.ndarray: Returns the sum of weights.
+        """
+        return self._instance.sum_of_weights()
+
+    def variance(self, ddof: int = 0) -> np.ndarray:
+        """
+        Returns the variance of samples.
+        
+        Args:
+            ddof (int, optional): Means Delta Degrees of Freedom. The divisor
+                used in calculations is N - ddof, where N represents the number
+                of elements. By default ddof is zero.
+
+        Returns:
+            numpy.ndarray: Returns the variance of samples.
+        """
+        return self._instance.variance()
