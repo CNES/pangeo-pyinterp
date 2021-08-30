26749fbc87558cd812665d00dde11db324fd3f07
Frédéric BRIOL <fbriol@groupcls.com>
2021-08-30 16:48:15 +0200

    bump to version 0.8.0

--- docs/source/changelog.rst
+++ docs/source/changelog.rst
@@ -1,7 +1,7 @@
 Changelog
 #########
 
-0.8.0 (XX YY 2021)
+0.8.0 (30 August 2021)
 ------------------
 * Calculates univariate statistics.
 * Binning using streaming histogram to evaluate quantiles.
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@ copyright = '(2021, CNES/CLS)'
 author = 'CNES/CLS'
 
 # The short X.Y version
-version = '0.7.1.dev21'
+version = '0.8.0'
 # The full version, including alpha/beta/rc tags
-release = '0.7.1.dev21'
+release = '0.8.0'
 
 # -- General configuration ---------------------------------------------------
 
