369685fe120d69570cac5244dd359b6f1cfdce8b
Frédéric BRIOL <fbriol@gmail.com>
2021-10-01 11:40:50 +0200

    :art: Imported module is not used.

--- src/pyinterp/tests/test_2d.py
+++ src/pyinterp/tests/test_2d.py
@@ -3,7 +3,6 @@
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 import collections
-import os
 import pickle
 import pytest
 import numpy as np
