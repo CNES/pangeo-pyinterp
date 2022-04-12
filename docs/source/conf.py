7606251bc4b5c335e62ad53d384d7cc4fb7f205f
Frédéric Briol <fbriol@gmail.com>
2022-04-12 16:22:32 +0200

    🔖 bump to version 0.14.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,9 @@ copyright = '(2022, CNES/CLS)'
 author = 'CNES/CLS'
 
 # The short X.Y version
-version = '0.13.0'
+version = '0.14.0'
 # The full version, including alpha/beta/rc tags
-release = '0.13.0'
+release = '0.14.0'
 
 # -- General configuration ---------------------------------------------------
 
