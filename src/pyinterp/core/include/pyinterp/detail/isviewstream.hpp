f5c4f4830934b00bf33d54311c4314a3d37e5c67
Frédéric Briol <fbriol@gmail.com>
2022-06-05 11:54:35 +0200

    🚨 Fix linter warnings.

--- src/pyinterp/core/include/pyinterp/detail/isviewstream.hpp
+++ src/pyinterp/core/include/pyinterp/detail/isviewstream.hpp
@@ -3,6 +3,7 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 #pragma once
+#include <algorithm>
 #include <istream>
 #include <streambuf>
 #include <string_view>
--- src/pyinterp/core/lib/geohash/base32.cpp
+++ src/pyinterp/core/lib/geohash/base32.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 CNES
+// Copyright (c) 2022 CNES
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
