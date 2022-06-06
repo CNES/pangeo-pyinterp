a9452333d330dda677a60eff9ea5ea7848a0745c
Frédéric BRIOL <fbriol@gmail.com>
2022-06-06 16:27:34 +0200

    🚨 Fix linter warnings.

--- src/pyinterp/core/include/pyinterp/detail/geometry/crossover.hpp
+++ src/pyinterp/core/include/pyinterp/detail/geometry/crossover.hpp
@@ -3,7 +3,10 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 #pragma once
+#include <algorithm>
+#include <limits>
 #include <optional>
+#include <set>
 #include <tuple>
 #include <utility>
 
