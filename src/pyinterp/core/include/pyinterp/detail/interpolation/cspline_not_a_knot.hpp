785b65dfa7c2342720c515d01ffb1b7060d56652
Frédéric BRIOL <fbriol@gmail.com>
2025-10-26 13:16:00 +0100

    refactor: Optimize compute_coefficients method in CSplineNotAKnot for performance and clarity

--- src/pyinterp/core/include/pyinterp/detail/interpolation/cspline_not_a_knot.hpp
+++ src/pyinterp/core/include/pyinterp/detail/interpolation/cspline_not_a_knot.hpp
@@ -3,7 +3,11 @@
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 #pragma once
+#include <Eigen/Core>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <limits>
+#include <vector>
 
 #include "pyinterp/detail/interpolation/cspline_base.hpp"
 
@@ -13,8 +17,13 @@ namespace pyinterp::detail::interpolation {
 template <typename T>
 class CSplineNotAKnot : public CSplineBase<T> {
  public:
-  using CSplineBase<T>::CSplineBase;
   using CSplineBase<T>::operator();
+
+  /// @brief Triplet type for sparse matrix construction
+  using Triplet = Eigen::Triplet<T>;
+
+  /// @brief Default constructor.
+  CSplineNotAKnot() : CSplineBase<T>(), triplets_{} {}
 
   /// Minimum number of data points required is 4.
   auto min_size() const -> Eigen::Index override { return 4; }
@@ -23,14 +32,18 @@ class CSplineNotAKnot : public CSplineBase<T> {
   /// Compute the spline coefficients (i.e. the first derivatives at the data
   /// points) by solving an \f$n\times n\f$ system with not‑a‑knot boundary
   /// conditions.
-  constexpr auto compute_coefficients(const Vector<T>& xa, const Vector<T>& ya)
+  auto compute_coefficients(const Vector<T>& xa, const Vector<T>& ya)
       -> bool override;
+
+ private:
+  /// @brief Member variable to avoid reallocations in repeated calls to
+  /// compute_coefficients
+  std::vector<Triplet> triplets_;
 };
 
 template <typename T>
-constexpr auto CSplineNotAKnot<T>::compute_coefficients(const Vector<T>& xa,
-                                                        const Vector<T>& ya)
-    -> bool {
+auto CSplineNotAKnot<T>::compute_coefficients(const Vector<T>& xa,
+                                              const Vector<T>& ya) -> bool {
   if (!Interpolator1D<T>::compute_coefficients(xa, ya)) {
     return false;
   }
@@ -38,48 +51,70 @@ constexpr auto CSplineNotAKnot<T>::compute_coefficients(const Vector<T>& xa,
   const auto size_m1 = size - 1;
   const auto size_m2 = size - 2;
 
-  // Resize the system: we now have n unknowns (one derivative per knot)
-  this->A_.resize(size, size);
-  this->b_.resize(size);
-  this->x_.resize(size);
-  this->A_.setZero();
-  this->b_.setZero();
-  this->x_.setZero();
+  // Use tolerance for numerical stability
+  constexpr T epsilon = std::numeric_limits<T>::epsilon() * T(10);
 
   // Compute step sizes: h_i = x[i+1] - x[i] for i = 0 ... n-2.
-  Vector<T> h(size_m1);
-  for (int64_t ix = 0; ix < size_m1; ++ix) {
-    h(ix) = xa(ix + 1) - xa(ix);
-    if (h(ix) == T(0)) {
-      throw std::invalid_argument("x-coordinates must be distinct");
-    }
+  // Vectorized computation for better performance
+  Eigen::Matrix<T, Eigen::Dynamic, 1> h = xa.tail(size_m1) - xa.head(size_m1);
+
+  // Check for duplicate x values
+  if ((h.array().abs() < epsilon).any()) {
+    return false;  // Two consecutive x values are too close.
   }
 
-  // Left not‑a‑knot condition at x0:
-  this->A_(0, 0) = -h(1);
-  this->A_(0, 1) = h(0) + h(1);
-  this->A_(0, 2) = -h(0);
+  // The not-a-knot boundary conditions create a banded matrix with bandwidth 3
+  this->b_.resize(size);
+  this->x_.resize(size);
+
+  // Not-a-knot boundary conditions result in b[0] = b[n-1] = 0
   this->b_(0) = T(0);
-
-  // Interior equations: for i = 1,..., n-2.
-  for (int64_t ix = 1; ix <= size_m2; ++ix) {
-    this->A_(ix, ix - 1) = h(ix - 1);
-    this->A_(ix, ix) = T(2) * (h(ix - 1) + h(ix));
-    this->A_(ix, ix + 1) = h(ix);
-    const T delta_i = (ya(ix + 1) - ya(ix)) / h(ix);
-    const T delta_im1 = (ya(ix) - ya(ix - 1)) / h(ix - 1);
-    this->b_(ix) = T(3) * (delta_i - delta_im1);
-  }
-
-  // Right not‑a‑knot condition at x[n-1]:
-  this->A_(size_m1, size - 3) = h(size_m2);
-  this->A_(size_m1, size_m2) = -(h(size_m2) + h(size - 3));
-  this->A_(size_m1, size_m1) = h(size_m2);
   this->b_(size_m1) = T(0);
 
-  // Solve the full system A * x_ = b_.
-  this->x_ = this->A_.colPivHouseholderQr().solve(this->b_);
-  return true;
+  // Compute divided differences: delta_i = (y[i+1] - y[i]) / h[i]
+  auto delta = (ya.tail(size_m1) - ya.head(size_m1)).array() / h.array();
+
+  // Compute interior values: b_i = 3 * (delta_i - delta_{i-1})
+  this->b_.segment(1, size_m2) =
+      T(3) * (delta.tail(size_m2) - delta.head(size_m2));
+
+  // Build the sparse system matrix A
+  triplets_.clear();
+  triplets_.reserve(3 * size);
+
+  // Row 0: Left not-a-knot condition
+  triplets_.emplace_back(0, 0, -h(1));
+  triplets_.emplace_back(0, 1, h(0) + h(1));
+  triplets_.emplace_back(0, 2, -h(0));
+
+  // Rows 1 to n-2: Standard cubic spline tridiagonal equations
+  for (int64_t i = 1; i <= size_m2; ++i) {
+    triplets_.emplace_back(i, i - 1, h(i - 1));
+    triplets_.emplace_back(i, i, T(2) * (h(i - 1) + h(i)));
+    triplets_.emplace_back(i, i + 1, h(i));
+  }
+
+  // Row n-1: Right not-a-knot condition
+  const auto h_nm2 = h(size_m2);
+  const auto h_nm3 = h(size - 3);
+  triplets_.emplace_back(size_m1, size - 3, h_nm2);
+  triplets_.emplace_back(size_m1, size_m2, -(h_nm2 + h_nm3));
+  triplets_.emplace_back(size_m1, size_m1, h_nm3);
+
+  // Solve the sparse linear system Ax = b
+  Eigen::SparseMatrix<T> A_sparse(size, size);
+  A_sparse.setFromTriplets(triplets_.begin(), triplets_.end());
+
+  Eigen::SparseLU<Eigen::SparseMatrix<T>> solver;
+  solver.compute(A_sparse);
+
+  if (solver.info() != Eigen::Success) {
+    return false;
+  }
+
+  this->x_ = solver.solve(this->b_);
+
+  return solver.info() == Eigen::Success;
 }
 
 }  // namespace pyinterp::detail::interpolation
