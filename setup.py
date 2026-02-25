2226e856bd0f5a1146901a03cf9a17f1db285ff1
Frédéric BRIOL <fbriol@gmail.com>
2026-02-25 15:40:23 +0100

    refactor: remove unnecessary build command from BuildExt class

--- setup.py
+++ setup.py
@@ -360,9 +360,6 @@ class BuildExt(setuptools.command.build_ext.build_ext):
 
         if configure:
             self.spawn(["cmake", str(WORKING_DIRECTORY), *cmake_args])
-        if not self.dry_run:
-            cmake_cmd = ["cmake", "--build", ".", "--target", "core"]
-            self.spawn(cmake_cmd + build_args)  # type: ignore[arg-type]
         os.chdir(str(WORKING_DIRECTORY))
 
     # pylint: enable=too-many-instance-attributes
